e30f414aa2106203dc6f0229ece30f78678f6544
Horacio Yeuchuk <horamda@gmail.com>
2026-02-17 12:36:57 -0300

    Add app=create_app

--- app.py
+++ app.py
@@ -171,6 +171,18 @@ def create_app():
      
     return app
 
+# ======================
+# Gunicorn binding
+# ======================
+
+app = create_app()
+
+
+# ======================
+# Desarrollo local
+# ======================
+
+
 if __name__ == "__main__":
     app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
